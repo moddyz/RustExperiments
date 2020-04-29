966354b2e4ae65322b40d90900b0595446921bc1
moddyz <moddy3d@gmail.com>
2020-04-29 20:54:22 +1200

    Added inline comments.

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -1,15 +1,15 @@
-use std::io;
+use std::io; // For standard input.
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // Create a mutable variable binding, from a string.
 
     io::stdin()
-        .read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line.");
+        .read_line(&mut guess) // Pass in guess as a _mutable_ reference.
+        .ok() // Discard any _potential_ error info.
+        .expect("Failed to read line."); // Panic with a message, if the Result from stdin was non-success.
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess); // {} for printing formatted arguments.
 }
