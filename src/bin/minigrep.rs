b8d15773debf764d5d9d011ac8d96b2e7a33c46a
moddyz <moddy3d@gmail.com>
2021-05-20 21:49:34 +1200

    Created config with constructor.

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -1,16 +1,28 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    pattern: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let pattern = args[1].clone();
+        let filename = args[2].clone();
+        Config { pattern, filename }
+    }
+}
+
 fn main() {
     // Collect command line arguments.
     let args: Vec<String> = env::args().collect();
 
-    let pattern = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", pattern);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.pattern);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Failed to read file");
+    let contents = fs::read_to_string(config.filename).expect("Failed to read file");
     println!("Text: {}", contents);
 }
